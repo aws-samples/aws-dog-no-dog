a3b3fc81fcb3ad21be0355acab51f910e41056b6
Nicolas Moutschen <moutschn@amazon.com>
2019-10-30 11:27:32 +0100

    Add automatic metrics from logs and add ENVIRONMENT variable

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,21 +1,6 @@
-import json
-import os
-
-
-DOMAIN_NAME = os.environ.get("DOMAIN_NAME", "*")
-
-
-def response(message, status_code=200):
-    return {
-        "statusCode": status_code,
-        "headers": {
-            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with",
-            "Access-Control-Allow-Origin": "{}".format(DOMAIN_NAME),
-            "Access-Control-Allow-Methods": "POST,GET,OPTIONS"
-        },
-        "body": json.dumps(message)
-    }
+import helpers
 
 
 def has_dog_handler(event, _):
-    pass
+    # TODO: implement logic here
+    return helpers.response("Hello, world!")
