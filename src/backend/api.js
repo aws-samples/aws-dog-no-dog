f73e96f82e123a91d131dac0972fa69c588270ca
Nicolas Moutschen <moutschn@amazon.com>
2019-11-19 21:49:10 +0100

    Comment mock implementation

--- src/backend/api.js
+++ src/backend/api.js
@@ -3,10 +3,10 @@ import { endpoint } from "@/config.js";
 
 // Mocking requests
 // You should comment this section once you have a backend running
-import MockAdapter from "axios-mock-adapter";
-let mock = new MockAdapter(axios, { delayResponse: 1000 });
-mock.onPost("/").reply(200, { dog: true });
-mock.onPost("/feedback").reply(200, { message: "Feedback received" });
+// import MockAdapter from "axios-mock-adapter";
+// let mock = new MockAdapter(axios, { delayResponse: 1000 });
+// mock.onPost("/").reply(200, { dog: true });
+// mock.onPost("/feedback").reply(200, { message: "Feedback received" });
 // End of the mock implementation section
 
 export function sendPhoto(photo) {
